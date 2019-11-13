2b071a9ff016fc1150417f49e8bc4e0e05f26dca
unknown <Avinash>
2019-11-13 11:24:48 +0530

    simple addf

--- Fibonaccy.java
+++ Fibonaccy.java
@@ -4,7 +4,7 @@ class Fibonaccy{
 	static Scanner sc=new Scanner(System.in);
             public static void main(String args[])
            {
-		int a=0,b=1,count=0;
+		int a=0,b=1,count;
 		System.out.println("Enter the Number:");
 		int n=sc.nextInt();
 		System.out.println(a);
