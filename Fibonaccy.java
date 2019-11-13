6269957958f69a12eb44abeb8705ee343d01908e
unknown <Avinash>
2019-11-13 11:19:08 +0530

    simple add

---
+++ Fibonaccy.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Fibonaccy{
+	
+	static Scanner sc=new Scanner(System.in);
+            public static void main(String args[])
+           {
+		int a=0,b=1,count=0;
+		System.out.println("Enter the Number:");
+		int n=sc.nextInt();
+		System.out.println(a);
+		System.out.println(b);
+		for(int i=1;i<=n;i++)
+		{	
+			count=a+b;
+			a=b;
+			b=count;
+			System.out.println(count);
+		}
+           }
+}
